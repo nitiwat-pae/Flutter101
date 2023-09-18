47fbc22a109315524647cb5172ba54964a309c60
Nitiwat <ch.nitiwat@gmail.com>
2023-09-18 21:57:26 +0700

    Update latest code

--- lib/src/pages/login/login_page.dart
+++ lib/src/pages/login/login_page.dart
@@ -11,41 +11,73 @@ class _LoginPageState extends State<LoginPage> {
   // Create new variable
   // final is non-editable variable
   final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
     _usernameController.text = "admin";
+    _passwordController.text = "1234";
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Login Page"),
+        title: Text("Login Flutter101"),
       ),
       body: Container(
         width: double.infinity,
         child: Padding(
           padding: const EdgeInsets.only(
               top: 32.0, left: 32.0, right: 32.0, bottom: 32.0),
-          child: Column(
-            children: [
-              TextField(
-                controller: _usernameController,
-                decoration: InputDecoration(labelText: "Username"),
+          child: Card(
+            child: Container(
+              padding: const EdgeInsets.all(32.0),
+              height: 310,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  ..._buildLoginFields(),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  ..._buildButtons()
+                ],
               ),
-              ElevatedButton(
-                  onPressed: _handleClickLogin,
-                  child: Text("Login"))
-            ],
+            ),
           ),
         ),
       ),
     );
   }
 
-  void _handleClickLogin() {
-    print("Login Flutter101");
+  void _handleClickLogin() => print(
+      "Login Flutter101: \n Username: (${_usernameController.text}) \n Password: ${_passwordController.text}");
+
+  void _handleClickReset() {
+    _usernameController.text = "";
+    _passwordController.text = "";
+    print("Username and Password field has been reset.");
+  }
+
+  _buildLoginFields() {
+    return [
+      TextField(
+        controller: _usernameController,
+        decoration: InputDecoration(labelText: "Username"),
+      ),
+      TextField(
+        controller: _passwordController,
+        decoration: InputDecoration(labelText: "Password"),
+      ),
+    ];
+  }
+
+  _buildButtons() {
+    return [
+      ElevatedButton(onPressed: _handleClickLogin, child: Text("Login")),
+      OutlinedButton(onPressed: _handleClickReset, child: Text("Reset"))
+    ];
   }
 }
